3339b5533b57847ef6c481db1fbfb6697029cfe0
TrinhHuuTho <trinhuutho@gmail.com>
2025-05-12 11:13:23 +0700

    Refactor TTS implementation: update .gitignore, clean up requirements, and enhance tts_view.py for better structure

--- models/tts_model.py
+++ models/tts_model.py
@@ -1,5 +1,5 @@
 # models/tts_model.py
-import edge_tts
+import edge_tts # type: ignore
 
 async def text_to_speech(text, lang="vi-VN", voice="vi-VN-HoaiMyNeural", output_path="output.mp3"):
     communicate = edge_tts.Communicate(text, voice)
--- views/tts_view.py
+++ views/tts_view.py
@@ -1,4 +1,8 @@
 # views/tts_view.py
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import streamlit as st
 from controllers.tts_controller import handle_tts_request
 
@@ -10,3 +14,6 @@ def tts_page():
         audio_path = handle_tts_request(text, lang)
         audio_file = open(audio_path, "rb")
         st.audio(audio_file.read(), format="audio/mp3")
+        
+if __name__ == "__main__":
+    tts_page()
